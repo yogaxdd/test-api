faf675a6dfafc50e1bbdd26df8cfbae3cdae4ba8
Yoga Rizki Pratama <110215272+yogaxdd@users.noreply.github.com>
2025-07-18 14:50:30 +0700

    Delete api/ip.js

--- api/ip.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
-    res.status(200).json({ ip: data.ip });
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal ambil IP server' });
-  }
-}
